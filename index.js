cc6ae3ba011398b555c72aa4a0ec67e7b992978c
Maadeha shaikh <maadeham@gmail.com>
2024-08-13 13:04:37 +0500

    update !

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
+// we also have to require path package as we are using path.join
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use(express.static(path.join(__dirname,'public')));
-// // this above line means that all the static files like images , stylesheets and javascript in sab ko find karny kay liyay ap publuc folder mai jana bus 
+app.use(express.static(path.join(__dirname,'public'))); //basically hm yeh keh rhy hain kay express ap staic files ko is path p dhondna (path.join(__dirname,'public')) and woh chala jay ga app.use mai like a middleware
+
+// this above line means that all the static files like images , stylesheets and javascript in sab ko find karny kay liyay ap publuc folder mai jana bus 
 // // let's understand what is __dirname it means that poora path apkay current folder tak ka aa jay ga and agar mai is kay sath /public jor don to yeh poora C:\Users\PMLS\Documents\Backend\Lecture 6 say C:\Users\PMLS\Documents\Backend\Lecture 6/public yeh ban jay ga ab agay / laga kay public folder bh sath lag jay ga .....
+
 // console.log(__dirname + '/public');
 // will give us C:\Users\PMLS\Documents\Backend\Lecture 6/public
 
-app.set('view engine','ejs'); // we are saying that our app ko setup kardo to show on frontend ejs pages simply
+app.set('view engine','ejs'); // we are saying that or app ko setup kardo to show on frontend ejs pages simply
 
 app.get("/", function(req,res){
    res.render("index"); // render matlb ab index ko lay kar aao 
